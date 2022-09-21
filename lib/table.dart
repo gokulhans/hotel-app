09175dc779969861a4ad4408cf045925123ea51c
govindhansv <govindhansv333@gmail.com>
2022-09-22 00:13:55 +0530

    tables listing

--- lib/table.dart
+++ lib/table.dart
@@ -32,7 +32,7 @@ class _TablepageState extends State<Tablepage> {
       ),
       body: StreamBuilder(
         // Reading Items form our Database Using the StreamBuilder widget
-        // stream: ,
+        stream: db.collection('orders').where("table",isEqualTo: widget.title ).snapshots(),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (!snapshot.hasData) {
             return const Center(
@@ -51,7 +51,89 @@ class _TablepageState extends State<Tablepage> {
                       child: Column(
                         children: [
                           ListTile(
-                            title: Text(documentSnapshot['tablepage']),
+                            title: Card(
+                              clipBehavior: Clip.antiAlias,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                   ListTile(
+                                    // leading: Icon(Icons.arrow_drop_down_circle),
+                                    title: Center(
+                                      child: Text(
+                                        documentSnapshot['name'],
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.w900,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                    ),
+                                    // subtitle: Text(
+                                    //   'Secondary Text',
+                                    //   style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                                    // ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Container(
+                                      height: 200,
+                                      alignment: Alignment.center,
+                                      decoration: BoxDecoration(
+                                          image: DecorationImage(
+                                        fit: BoxFit.cover,
+                                        image: NetworkImage(documentSnapshot['image']),
+                                      )),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(16.0),
+                                    child: Text(
+                                      documentSnapshot['des'],
+                                      style: TextStyle(
+                                          color: Colors.black.withOpacity(0.6)),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 2),
+                                    child: ButtonBar(
+                                      alignment: MainAxisAlignment.start,
+                                      children: [
+                                        ElevatedButton.icon(
+                                          icon: const Icon(Icons.delete_rounded,
+                                              size: 18),
+                                          label: const Text("Order Ready"),
+                                          onPressed: () {},
+                                        ),
+                                        ElevatedButton.icon(
+                                          icon:
+                                              const Icon(Icons.edit, size: 16),
+                                          label: const Text("Done"),
+                                          onPressed: () {
+                                            db
+                                                .collection('orders')
+                                                .doc(documentSnapshot.id)
+                                                .delete();
+                                          },
+                                        ),
+                                         ElevatedButton(
+                                         
+                                          onPressed: () {
+                                            
+                                          },
+                                          child: Text("count " + documentSnapshot['count']),
+                                        ),
+                                         ElevatedButton(
+                                         
+                                          onPressed: () {
+                                            
+                                          },
+                                          child: Text("table "+ documentSnapshot['table']),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
                           ),
                         ],
                       ),
